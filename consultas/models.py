083b1799a5189ef47b8476c3c579d42116ea00de
Gerson-Lima <gersondouglas2011@gmail.com>
2024-12-05 14:52:52 -0300

    feature: consultas

--- consultas/models.py
+++ consultas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from users.models import Medico
+
 
 class Consulta(models.Model):
     paciente = models.CharField(max_length=100)
-    medico = models.CharField(max_length=100)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
     data_horario = models.DateTimeField()
     especialidade = models.CharField(max_length=100)
     observacoes = models.TextField(blank=True, null=True)
